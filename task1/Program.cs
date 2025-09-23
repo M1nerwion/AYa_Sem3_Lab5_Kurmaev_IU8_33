4d2edbd0ae80822718097bcc84453595dae249a9
M1nerwion <kyrmaevivan06@mail.ru>
2025-09-23 23:40:15 +0300

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,139 @@
+﻿namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число столбцов:");
+            int m = Convert.ToInt32(Console.ReadLine());
+            // int m = 3;//Число строк в матрице
+
+            Console.WriteLine("Введите число строк:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            //int n = 3;//Число столбцов в матрице
+
+            Console.WriteLine("Введите минимальное число диапозона генерации псевдослучайных чисел:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            //int a = 10;
+
+            Console.WriteLine("Введите максимальное число диапозона генерации псевдослучайных чисел:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            //int b = 100;
+
+            Console.WriteLine("Матрица 1");
+            MyMatrix mx = new MyMatrix(m, n, a, b);//Создаем нашу матрицу
+            mx.Show();//Выводим матрицу в консоль
+
+            Console.WriteLine("Перезаполняем матрицу 1");
+            mx.Fill(a, b);//Перезаполняем матрицу числами
+            mx.Show();//Выводим матрицу в консоль
+
+            Console.WriteLine("Меняем размер матрицы 1 на 2х2");
+            mx.ChangeSize(2, 2, a, b);//Меняем размер матрицы
+            mx.Show();// Выводим матрицу в консоль
+
+            Console.WriteLine("Меняем размер матрицы 1 на 3х4");
+            mx.ChangeSize(3, 4, a, b);//Меняем размер матрицы
+            mx.Show();// Выводим матрицу в консоль
+
+            Console.WriteLine("Подматрица матрицы 1 расположенная в центре");
+            mx.ShowPartialy(2, 2, 2, 3);//Выводим в консоль подматрицу исходной
+        }
+    }
+}
+
+class MyMatrix
+{
+    int m = 0;
+    int n = 0;
+    int[,] mas;
+    public MyMatrix(int m, int n, int a, int b)
+    {
+        this.m = m;
+        this.n = n;
+
+        mas = new int[m, n];
+        Random rand = new();
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                mas[i, j] = rand.Next(a, b);
+            }
+        }
+    }
+    public void Fill(int a, int b)//метод Fill, перезаполняющий матрицу случайными значениями.
+    {
+        Random rand = new();
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                mas[i, j] = rand.Next(a, b);
+            }
+        }
+    }
+
+    public void ChangeSize(int m1, int n1, int a, int b)//метод ShowPartialy, принимающий в качестве параметров начальные и конечные значения строк и столбцов, значения матрицы внутри которых нужно вывести на консоль.
+    {
+        int[,] mas1 = new int[m1, n1];
+        Random rand = new();
+
+        for (int i = 0; i < m1; i++)
+        {
+            for (int j = 0; j < n1; j++)
+            {
+                if ((i < m) && (j < n))
+                {
+                    mas1[i, j] = mas[i, j];
+                }
+                else
+                {
+                    mas1[i, j] = rand.Next(a, b);
+                }
+            }
+        }
+
+        mas = mas1;
+        m = m1;
+        n = n1;
+    }
+
+    public void ShowPartialy(int m1, int m2, int n1, int n2)//метод ShowPartialy, принимающий в качестве параметров начальные и конечные значения строк и столбцов, значения матрицы внутри которых нужно вывести на консоль.
+    {
+        if (!((((0 <= m1) && (m1 <= m)) && ((0 <= m2) && (m2 <= m))) && (((0 <= n1) && (n1 <= n)) && ((0 <= n2) && (n2 <= n)))))
+        {
+            throw new ArgumentOutOfRangeException();//Если пытаемся получить подтаблицу вне диапозона исходной, то генерируем  исключение
+        }
+
+        for (int i = m1 - 1; i < m2; i++)
+        {
+            for (int j = n1 - 1; j < n2; j++)
+            {
+                Console.Write($" {mas[i, j]} ");
+            }
+            Console.Write("\n");
+        }
+        Console.Write("\n");
+    }
+
+    public void Show()//метод Show, выводящий все значения матрицы на консоль.
+    {
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write($" {mas[i, j]} ");
+            }
+            Console.Write($"\n");
+        }
+        Console.Write("\n");
+    }
+
+    public int this[int i, int j]//Индексатор
+    {
+        get => mas[i, j];
+        set => mas[i, j] = value;
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> myList = new MyList<string>();
+
+            myList.Add("Первый элемент");
+            Console.WriteLine(myList.Count);
+
+            myList.Add("Второй элемент");
+            Console.WriteLine(myList.Count);
+
+            Console.Write("Первый элемент списка: ");
+            Console.WriteLine(myList[0]);
+
+            myList.Print();
+
+           // MyList<int> myList1Int = new MyList<int>() { 10, 15, 20 };//Доработать IEnumerator
+        }
+    }
+}
+
+class MyList<T> //:IEnumerable<T>
+{
+    public Element<T> first;
+
+    private int _Count;
+    public int Count { get => _Count; }//Свойство получения общего количества элементов
+
+    public MyList(params T?[] elements)
+    {
+        first = new Element<T>(0);
+
+        _Count = 0;
+        for (int i = 0; i < elements.Length; i++)
+        {
+            if ((i == 0) && (Count == 0))
+            {
+                first.Value = elements[i];//Если первый элемент в списке, то достаточно поменять значение по умолчанию у созданного по умолчанию первого элемента
+            }
+            else
+            {
+                Add(elements[i]);//Если не первый элемент, то просто добавить в список
+            }
+        }
+        _Count = elements.Length;
+    }
+
+
+    public class Element<K>
+    {
+        public Element<K>? Next;
+        public int Number { get; }
+
+        private K? _value;
+
+        public K? Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+
+        public Element(int Number)
+        {
+            Next = default;
+            this.Number = Number;
+            _value = default;
+        }
+        public Element(int Number, K? _value)
+        {
+            Next = default;
+            this.Number = Number;
+            this._value = _value;
+        }
+    }//Класс элементов
+
+    //class ListEnumerator : IEnumerator<Element<T>> {
+    //{
+    //    public bool MoveNext()
+    //    {
+    //        if 
+    //    }
+    //}
+
+    //public IEnumerator<T> GetEnumerator() => days.GetEnumerator();
+
+    public void Add(T? _value)//Добавление элемента в список
+    {
+        Element<T> element = new Element<T>(Count, _value);
+
+        if (Count == 0) { first = element; }
+        else
+        {
+            Element<T>? PreviusElement = GetElement(Count - 1);
+            PreviusElement?.Next = element;
+        }
+        _Count++;
+    }
+
+    public T? this[int index]
+    {
+        get
+        {
+            Element<T>? temp = first;
+            if (index >= Count) { throw new IndexOutOfRangeException(); }
+            for (int i = 0; i < index; i++)
+            {
+                temp = first.Next;
+            }
+            return temp.Value;
+        }
+        set 
+        {
+            Element<T>? temp = first;
+            if (index >= Count) { throw new IndexOutOfRangeException(); }
+            for (int i = 0; i < index; i++)
+            {
+                temp = first.Next;
+            }
+            temp?.Value = value;
+        }
+    }//Индексатор, возвращает значение элемента списка
+
+    private Element<T>? GetElement(int index)
+    {
+        Element<T>? temp = first;
+        if (index >= Count) { throw new IndexOutOfRangeException(); }
+        for (int i = 0; i < index; i++)
+        {
+            temp = first.Next;
+        }
+        return temp;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine("\nВесь список:");
+        for (int i = 0; i < this.Count; i++)
+        {
+            Console.WriteLine(this[i]);
+        }
+        Console.WriteLine();
+    }
+}
